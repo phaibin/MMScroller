5b3f216e638af9e78f2be37720f6034d6cc33c86
Leon <phaibin@gmail.com>
2011-10-02 10:52:16 +0800

    init

---
+++ MMScroller/MMScrollerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  MMScrollerAppDelegate.h
+//  MMScroller
+//
+//  Created by Leon on 10/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MMScrollerAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MMScroller/MMScrollerAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  MMScrollerAppDelegate.m
+//  MMScroller
+//
+//  Created by Leon on 10/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MMScrollerAppDelegate.h"
+
+@implementation MMScrollerAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
